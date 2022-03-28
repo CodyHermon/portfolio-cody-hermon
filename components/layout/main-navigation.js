5eda52ef1ea0ac04296296e8b0dc77eabbfa2b01
Cody Hermon <codyhermon@gmail.com>
2022-03-28 13:19:28 -0700

    active tab css

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,13 +1,36 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import classes from './main-navigation.module.css';
 
 const MainNavigation = () => {
+  const router = useRouter();
+
   return (
     <header className={classes.header}>
-      <Link href='/'>Home</Link>
-      <Link href='/experience'>Experience</Link>
-      <Link href='/contact'>Contact</Link>
+      <Link href='/'>
+        <a className={router.pathname === '/' ? `${classes.activeTab}` : ''}>
+          Home
+        </a>
+      </Link>
+      <Link href='/experience'>
+        <a
+          className={
+            router.pathname === '/experience' ? `${classes.activeTab}` : ''
+          }
+        >
+          Experience
+        </a>
+      </Link>
+      <Link href='/contact'>
+        <a
+          className={
+            router.pathname === '/contact' ? `${classes.activeTab}` : ''
+          }
+        >
+          Contact
+        </a>
+      </Link>
     </header>
   );
 };
