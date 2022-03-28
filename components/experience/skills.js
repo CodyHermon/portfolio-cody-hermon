606514150431649fc7993d67c234f57046dd6138
Cody Hermon <codyhermon@gmail.com>
2022-03-28 14:31:47 -0700

    Update main-navigation.module.css

--- components/experience/skills.js
+++ components/experience/skills.js
@@ -6,21 +6,27 @@ const Skills = () => {
       <h2>Skills</h2>
       <ul className={classes.skills}>
         <li>
+          <p>React</p>
           <i className='devicon-react-original'></i>
         </li>
         <li>
+          <p>Nextjs</p>
           <i className='devicon-nextjs-original'></i>
         </li>
         <li>
+          <p>Javascript</p>
           <i className='devicon-javascript-plain'></i>
         </li>
         <li>
+          <p>HTML</p>
           <i className='devicon-html5-plain'></i>
         </li>
         <li>
+          <p>CSS</p>
           <i className='devicon-css3-plain'></i>
         </li>
         <li>
+          <p>C#</p>
           <i className='devicon-csharp-plain'></i>
         </li>
       </ul>
